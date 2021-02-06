2ff803179640a5d03ab0bd6b2ed3942f35bc156d
ErdalKARAER <e.karaer@lprs.fr>
2021-02-06 22:42:03 +0100

    changement css

--- vue/form_connexion.php
+++ vue/form_connexion.php
@@ -39,15 +39,16 @@
           <div class="col-xl-9 mx-auto">
             <div class="cta-inner text-center rounded">
               <ul class="list-unstyled list-hours mb-5 text-left mx-auto">
-                <div class="form_inscription">
+                <div class="form_connexion">
                 <h1>Connectez-vous : </h1>
                 <form action="../traitement/traitement_co.php" method="post">
                   <p>
                     Mail : <input type="text" name="email"required/><br>
-                    Mot de passe : <input type="password" name="pwd"required/><br>
+                    <br>
+                      Mot de passe : <input type="password" name="pwd"required/><br>
                     <br>
                   </p>
-                    <input type="submit" value="Inscription"/><br>
+                    <input type="submit" value="Connexion"/><br>
                   </form>
                 </div>
               </ul>
