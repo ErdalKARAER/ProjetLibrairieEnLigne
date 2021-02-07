eaf65e760448defc5f7fee1befde404d39c93cfb
ErdalKARAER <e.karaer@lprs.fr>
2021-02-07 11:36:52 +0100

    include

---
+++ include/titre.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<h1 class="site-heading text-center text-white d-none d-lg-block">
+  <span class="site-heading-upper text-primary mb-3">Librairie en ligne</span>
+  <span class="site-heading-lower">Bienvenue</span>
+</h1>
+</html>
